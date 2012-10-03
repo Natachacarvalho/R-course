b83c90e7f3674698224c7359633699122e59f965
Paul Campbell <pcsnow@gmal.com>
2012-10-02 19:40:54 -0700

    initial

---
+++ corr.R
@@ -0,0 +1,58 @@
+        # Write a function that reads a directory full of files and reports the number of
+        # completely observed cases in each data file.
+        # The function should return a data frame
+        # where the first column is the name of the file and the
+        # second column is the number of complete cases.
+        # A prototype of this function follows
+
+
+complete <- function(directory, id = 1:332) {
+        ## directory' is a character vector of length 1 indicating
+        ## the location of the CSV files
+
+        ## id is an integer vector indicating the monitor ID numbers
+        ## to be used
+
+        ## Return a data frame of the form:
+        ## id nobs
+        ## 1  117
+        ## 2  1041
+        ## ...
+        ## where id is the monitor ID number and 'nobs' is the
+        ## number of complete cases
+
+        NL <- "\n"
+        CSV <- ".csv"
+        SLASH <- "/"
+        ID <- "id"
+        NOBS <- "nobs"
+
+
+        idVector <- vector(mode = "character", length = 0)
+        nobVector <- vector(mode = "character", length = 0)
+
+        for ( i in id ) {
+
+        ## Combine id and directory to create a full name
+            idchar <- as.character(formatC(i, width=3, format='d', flag=0))
+            filename <- paste0( idchar , CSV )
+            fullname <- paste0(directory, SLASH, filename)
+
+        ## extract from csv and create a frame
+
+        air_data <- data.frame(list(read.csv( fullname )))
+
+        ## omit lines with NA
+        ds_na_omit <- na.omit( air_data )
+
+        # add elements to  vector
+        idVector <- append(idVector, as.character(i) )
+        nobVector<- append( nobVector, as.character(nrow(ds_na_omit)))
+        }
+         # create frame from the traversal of the id's and label columnes
+           nonNA_report <- data.frame( idVector,nobVector  )
+           colnames( nonNA_report ) <- c("id", "nobs")
+
+          # complete
+        return (nonNA_report)
+      }
