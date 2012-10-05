61074a815a91a77961ad4d659e473fa3e17ce839
Paul Campbell <pcsnow@gmal.com>
2012-10-05 15:59:24 -0700

    got correlation working

--- corr.R
+++ corr.R
@@ -1,58 +1,75 @@
-        # Write a function that reads a directory full of files and reports the number of
-        # completely observed cases in each data file.
-        # The function should return a data frame
-        # where the first column is the name of the file and the
-        # second column is the number of complete cases.
-        # A prototype of this function follows
 
 
-complete <- function(directory, id = 1:332) {
-        ## directory' is a character vector of length 1 indicating
+corr <- function(directory, threshold = 0) {
+        ## 'directory' is a character vector of length 1 indicating
         ## the location of the CSV files
 
-        ## id is an integer vector indicating the monitor ID numbers
-        ## to be used
+        ## 'threshold' is a numeric vector of length 1 indicating the
+        ## number of completely observed observations (on all
+        ## variables) required to compute the correlation between
+        ## nitrate and sulfate; the default is 0
 
-        ## Return a data frame of the form:
-        ## id nobs
-        ## 1  117
-        ## 2  1041
-        ## ...
-        ## where id is the monitor ID number and 'nobs' is the
-        ## number of complete cases
+        ## Return a numeric vector of correlations
 
         NL <- "\n"
-        CSV <- ".csv"
-        SLASH <- "/"
+
         ID <- "id"
         NOBS <- "nobs"
 
 
+
+
+
         idVector <- vector(mode = "character", length = 0)
         nobVector <- vector(mode = "character", length = 0)
+        sulphurVector<- vector( mode="numeric", length=0)
+        nitrateVector<- vector( mode="numeric", length=0)
+        corrVector<- vector( mode="numeric", length=0)
+        id <- c(1:2 )                                        #Testing
+        traceback()
+        #debug(get_data)
+        #debug(create_fullname)
 
         for ( i in id ) {
 
-        ## Combine id and directory to create a full name
-            idchar <- as.character(formatC(i, width=3, format='d', flag=0))
-            filename <- paste0( idchar , CSV )
-            fullname <- paste0(directory, SLASH, filename)
+         fullname <- create_fullname ( i , directory )
 
         ## extract from csv and create a frame
 
-        air_data <- data.frame(list(read.csv( fullname )))
+        cat("fullname: ", fullname, NL)                ##DEBUG
 
-        ## omit lines with NA
+        air_data <- get_data( fullname)
         ds_na_omit <- na.omit( air_data )
+        cat("ds_na_omit:", NL)
+        str(ds_na_omit)
 
-        # add elements to  vector
-        idVector <- append(idVector, as.character(i) )
-        nobVector<- append( nobVector, as.character(nrow(ds_na_omit)))
-        }
+        cat("howmany : ", length(ds_na_omit$sulfate), " , ", length(ds_na_omit$nitrate), NL)
+        ds_na_omit$corr <-  cor( as.numeric(ds_na_omit$sulfate), y=ds_na_omit$nitrate )
+        # did I get good data ?
+
+        cat( "frame after cor : ", NL)
+        str(ds_na_omit)
+
+         }
+
          # create frame from the traversal of the id's and label columnes
-           nonNA_report <- data.frame( idVector,nobVector  )
-           colnames( nonNA_report ) <- c("id", "nobs")
+           ds_na_omit <- data.frame( idVector,nobVector  )
+           colnames( ds_na_omit ) <- c( ID, NOBS )
 
           # complete
-        return (nonNA_report)
-      }
+        return (ds_na_omit)
+      }        # end of corr
+
+       get_data <- function(fullname)  {
+            data.frame( ( list(read.csv( fullname ))) )
+        }
+
+        create_fullname <- function( i, directory ) {
+        CSV <- ".csv"
+        SLASH <- "/"
+         ## Combine id and directory to create a full name
+         idchar <- as.character(formatC(i, width=3, format='d', flag=0))
+         filename <- paste0( idchar , CSV )
+         returnValue<- paste0(directory, SLASH, filename)
+        }
+
