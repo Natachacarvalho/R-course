b08896d7bc1ea002870501ac154153380488affa
Paul Campbell <pcsnow@gmal.com>
2012-10-10 12:15:04 -0700

    initial histogram

---
+++ plot1.R
@@ -0,0 +1,4 @@
+outcome <- read.csv("outcome-of-care-measures.csv", colClasses= "character" )
+outcome[, 11] <- as.numeric(outcome[,11] )
+hist ( outcome[,11] , main = "Heart Attack 30-day Death Rate", ylab="Frequency",
+    xlab="30-day Death Rate"  )
