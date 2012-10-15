3861bf1f91a5849a029b573e66c5dd6a136dd508
Paul Campbell <pcsnow@gmail.com>
2012-10-15 13:59:53 -0700

    Added function for primative check state.

--- best.R
+++ best.R
@@ -1,5 +1,12 @@
 best <- function(state, outcome_name ){ 
-  print ( str( state ) )
+  
+   if (!checkstate(state))  stop ("invalid state")
   
   print ( str( outcome_name ) )
+  
   }
+
+checkstate<- function(state) {
+  if (!is.character(state) |  nchar(state) != 2 ){ return (
+    return (
+}
