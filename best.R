8e9bd7e7ed2aae96062814779ca8da75772c9fb7
Paul Campbell <pcsnow@gmail.com>
2012-10-15 14:43:34 -0700

    added functions for arguments

--- best.R
+++ best.R
@@ -1,12 +1,37 @@
-best <- function(state, outcome_name ){ 
+best <- function(state, outcome_name , num = "best"){ 
   
-   if (!checkstate(state))  stop ("invalid state")
-  
-  print ( str( outcome_name ) )
+  checkarguments(state, outcome_name , num)
+
   
   }
 
 checkstate<- function(state) {
-  if (!is.character(state) |  nchar(state) != 2 ){ return (
-    return (
+  NL <- "\n"
+  if (!is.character(state) ){ return (FALSE) }
+  
+  if (nchar(state) == 2 ) { 
+       return(TRUE)
+    }
+    else { return (FALSE) } #should not occur
+}
+checkoutcome<-function(outcome_name) {
+  if (outcome_name == "Heart Attack" 
+      | outcome_name == "Heart Failure" 
+      | outcome_name == "Pneumonia" )
+    return (TRUE) 
+  
+  else  return (FALSE)
+} 
+  checknum<-function(num) {
+    if (is.numeric(num)) return (TRUE)   ##TODO need to check for match state count
+    if (!is.character( num )) return (FALSE)   
+    if (num == "best" | num == "worst"  )  return (TRUE)
+    return (FALSE)
+  }
+checkarguments<-function (state, outcome_name , num ){
+ 
+  if (!checkstate(state))  stop ("invalid state")
+  if (!checkoutcome(outcome_name))  stop ("invalid outcome")
+  if (!checknum(num))  stop ("invalid num")
+  
 }
