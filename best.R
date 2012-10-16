c98b5e500f1ac77aba529051e6f7e53c56b461af
Paul Campbell <pcsnow@gmail.com>
2012-10-16 16:35:49 -0700

    was stuck on NUlls but they came from print(). Code is ready to start selecting states and conditions

--- best.R
+++ best.R
@@ -3,11 +3,27 @@ NL <<- "\n"
 best <- function(state, outcome_name ){ 
   
   checkarguments(state, outcome_name )
- 
+  
+  debug(readoutcome)
   outcome <- readoutcome()
-
+  
+  print("before str")
+  str(outcome)
+  #diminished.outcome <- parse.data(outcome, state,outcome_name)
+  print ("after parse")
   }
-
+readoutcome<-function(){
+  value<-read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  
+  table <- data.frame( value, stringsAsFactors = FALSE)
+  table[,11] <- as.numeric(table[,11]    )
+  table[,17] <- as.numeric(table[,17]    )
+  table[,23] <- as.numeric(table[,23]    )
+  str(table)
+  return(table)
+  
+  # add selection / subset
+}
 
 checkstate<- function(state) {
   if (!is.character(state) ){ return (FALSE) }  
@@ -16,7 +32,7 @@ checkstate<- function(state) {
     else { return (FALSE) } #should not occur
 }
 checkoutcome<-function(outcome_name) {
-  if (outcome_name == "heart attack" 
+  if  ( outcome_name == "heart attack" 
       | outcome_name == "heart failure" 
       | outcome_name == "pneumonia" )
     return (TRUE)   
@@ -27,13 +43,10 @@ checkarguments<-function (state, outcome_name ) {
   if (!checkoutcome(outcome_name))  stop ("invalid outcome")
   if (!checkstate(state)) top ("invalid state")
 }
-readoutcome<-function(){
-  value<-read.csv("outcome-of-care-measures.csv", colClasses = "character")
-}
+
 checkstate<- function(state) {
   if (!is.character(state) ){ return (FALSE) }  
   if (nchar(state) == 2 ) { 
     return(TRUE)}
   else { return (FALSE) } #should not occur
 }
-
