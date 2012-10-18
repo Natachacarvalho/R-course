46f4d701974d511e0b2efd9bb70e3353de3dae79
Paul Campbell <pcsnow@gmail.com>
2012-10-18 12:46:17 -0700

    reverted some code

--- best.R
+++ best.R
@@ -1,40 +1,33 @@
 
 NL <<- "\n"
 outcomeSTRINGS <<- c( "heart attack","heart failure" ,"pneumonia")
-outcomeINDEX <<-   c( 11, 17,23 )
+outcomeINDEX <<-   c( 9,11, 17,23 )
 best <- function(state, outcome_name ){ 
   
   checkarguments(state, outcome_name )
   
   outcome <- readoutcome(state)
-      names(outcome)[11] <- "mortHA"
-  listname <- names(outcome)
+      
+  colnames(outcome)[11] <- "mortHA"
   
   
-  cat("name for ,11 ", listname, NL)
-  
-  #results <- parse.data(outcome,  outcome_name)
- 
+  results <- parse.data(outcome,  outcome_name)
+   
+  print(subset( results, select=c( County.Name,Hospital.Name, mortHA))  )
   }
 
 parse.data<- function (out, outcome_name) {
-  
-  out.sorted <- out[order(,out$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack  )]
-
-  #str(out.sorted)
+  # out$Hospital.Name,
+  out.sorted <- out[order(out$mortHA  ),]
 }
 readoutcome<-function(state){
-  value<-read.csv("outcome-of-care-measures.csv", colClasses = "character")
-  
+  value<-read.csv("outcome-of-care-measures.csv", colClasses = "character")  
   table <- data.frame( value, stringsAsFactors = FALSE)
-  table[,11] <- as.numeric(table[,11]    )
-  table[,17] <- as.numeric(table[,17]    )
-  table[,23] <- as.numeric(table[,23]    )
+  table[,11] <- as.numeric(gsub("Not Available", NA, table[,11]))
+  table[,17] <- as.numeric(gsub("Not Available", NA, table[,17]))
+  table[,23] <- as.numeric(gsub("Not Available", NA, table[,23]))
   table <- subset(table, State == state)
-  
   return(table)
-  
-  # add selection / subset
 }
 
 checkstate<- function(state) {
@@ -44,11 +37,11 @@ checkstate<- function(state) {
     else { return (FALSE) } #should not occur
 }
 checkoutcome<-function(outcome_name) {
-  if  ( outcome_name == "heart attack" 
-      | outcome_name == "heart failure" 
-      | outcome_name == "pneumonia" )
-    return (TRUE)   
-  else  return (FALSE)
+   if  ( outcome_name == "heart attack" 
+       | outcome_name == "heart failure" 
+       | outcome_name == "pneumonia" )
+     return (TRUE)   
+   else  return (FALSE)
 } 
 
 checkarguments<-function (state, outcome_name ) { 
